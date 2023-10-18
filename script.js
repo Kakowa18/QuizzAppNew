1b395172c3ef07d58e840b377d62a4c609b7ac8d
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2023-10-18 20:12:35 +0200

    added new questions

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ let questions = [
     {
         'question': 'Welche Programmiersprache wird weltweit am häufigsten genutzt? (stand: 2022)',
         'answer_1': 'Java',
-        'answer_2': 'Javascript',
-        'answer_3': 'Python',
+        'answer_2': 'Python',
+        'answer_3': 'Javascript',
         'answer_4': 'C#',
-        'right_answer': 3
+        'right_answer': 2
     },
     {
         'question': 'Welcher Browser hat nach Chrome den größten Marktanteil weltweit? (stand: 2022)',
@@ -35,11 +35,17 @@ let questions = [
 
 let curentQuestion = 0;
 
+
 function init() {
     let lengthOfQuestions = document.getElementById('length-of-four');
     lengthOfQuestions.innerHTML = `${questions.length}`;
 
     showQuestion();
+    disableBtn();
+}
+
+function disableBtn() {
+    document.getElementById('nextQuestion').disabled = true;
 }
 
 
@@ -59,17 +65,53 @@ function showQuestion() {
 }
 
 function clickOnButton(selection) {
-   
-
+    document.getElementById('nextQuestion').disabled = false;
     let answer = document.getElementById(selection);
     let lastCharacter = selection.slice(-1);
+
+    let greenAnswer = document.getElementById('answer_3');
     
-    if( lastCharacter  == 3) {
+    if(lastCharacter  == questions[curentQuestion]['right_answer']) {
         answer.classList.add('new-background-color-green');
         answer.classList.add('new-background-color-green:hover');
     } else {
         answer.classList.add('new-background-color-red');
         answer.classList.add('new-background-color-red:hover');
+        greenAnswer.classList.add('new-background-color-green')
     }
 }
 
+
+
+function nextQuestion () {
+    curentQuestion++;
+    document.getElementById('current-question-number').innerHTML = `${curentQuestion+1} `;
+    document.getElementById('nextQuestion').disabled = true;
+    let currentQuestionText = document.getElementById('current-question');
+    currentQuestionText.innerHTML = `<h2>${questions[curentQuestion]['question']}</h2>`;
+
+    let firstAnswer = document.getElementById('answer_1');
+    let secondAnswer = document.getElementById('answer_2');
+    let thirdAnswer = document.getElementById('answer_3');
+    let fourthAnswer = document.getElementById('answer_4');
+
+    firstAnswer.innerHTML = `${questions[curentQuestion]['answer_1']}`;
+    secondAnswer.innerHTML = `${questions[curentQuestion]['answer_2']}`;
+    thirdAnswer.innerHTML = `${questions[curentQuestion]['answer_3']}`;
+    fourthAnswer.innerHTML = `${questions[curentQuestion]['answer_4']}`;
+
+    resetColor();
+}
+
+function resetColor() { 
+    document.getElementById('answer_1').classList.remove('new-background-color-green');
+    document.getElementById('answer_1').classList.remove('new-background-color-red');
+    document.getElementById('answer_2').classList.remove('new-background-color-green');
+    document.getElementById('answer_2').classList.remove('new-background-color-red');
+    document.getElementById('answer_3').classList.remove('new-background-color-green');
+    document.getElementById('answer_3').classList.remove('new-background-color-red');
+    document.getElementById('answer_4').classList.remove('new-background-color-green');
+    document.getElementById('answer_4').classList.remove('new-background-color-red');
+
+    disableBtn();
+}
